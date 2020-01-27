b4c9521094a610898753bdb766bec9ba5065a9b2
Lrakotoson <rakotosonloicandoniaina@gmail.com>
2020-01-27 23:40:46 +0100

    setup.py for Pypi

--- setup.py
+++ setup.py
@@ -8,12 +8,25 @@ with open('requirements.txt') as f:
 
 setup(
     name = "Jobtimize",
+    packages = ["Jobtimize"],
     version = Jobtimize.__version__,
     install_requires = requirements,
     author = "Loïc Rakotoson",
     author_email = "contact@loicrakotoson.com",
+    url = "https://github.com/Lrakotoson/Jobtimize",
     description = "Collect and standardize data on job posting platforms.",
     long_description = open("README.md").read(),
     long_description_content_type = "text/markdown",
+    license = "MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 1 - Planning",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities"
+    ],
 
 )
